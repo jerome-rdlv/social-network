608df0bb42d7407b68a0ad542ebb47170c3d4026
Jérôme Mulsant <jerome@rue-de-la-vieille.fr>
2017-09-07 10:20:05 +0200

    Update bootstrap namespace

--- wp-social-network.php
+++ wp-social-network.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace Rdlv\WordPress\Networks;
-
 /*
 Plugin Name: Advanced Custom Fields: Facebook Connection
 Plugin URI: PLUGIN_URL
@@ -14,86 +12,82 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
 // exit if accessed directly
+use Rdlv\WordPress\Networks\NetworkApi;
 
-
-if( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 
 // check if class already exists
-if( !class_exists('Rdlv\WordPress\Networks\Networks') ) :
+if (!class_exists('WpSocialNetwork')) :
 
-class Networks {
-	
-	/*
-	*  __construct
-	*
-	*  This function will setup the class functionality
-	*
-	*  @type	function
-	*  @date	17/02/2016
-	*  @since	1.0.0
-	*
-	*  @param	n/a
-	*  @return	n/a
-	*/
+    class WpSocialNetwork
+    {
 
-    /**
-     * Networks constructor.
-     */
-    function __construct() {
-		
-		// vars
-		$this->settings = array(
-			'version'	=> '1.0.0',
-			'url'		=> plugin_dir_url( __FILE__ ),
-			'path'		=> plugin_dir_path( __FILE__ )
-		);
-		
-		
-		// set text domain
-		// https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
-		load_plugin_textdomain( 'acf-social_connection', false, plugin_basename( dirname( __FILE__ ) ) . '/lang' );
-		
-		
-		// include field
-		add_action('acf/include_field_types', 	array($this, 'include_field_types')); // v5
-		add_action('acf/register_fields', 		array($this, 'include_field_types')); // v4
+        /*
+        *  __construct
+        *
+        *  This function will setup the class functionality
+        *
+        *  @type	function
+        *  @date	17/02/2016
+        *  @since	1.0.0
+        *
+        *  @param	n/a
+        *  @return	n/a
+        */
 
-        NetworkApi::init();
+        /**
+         * Networks constructor.
+         */
+        function __construct()
+        {
+            // vars
+            $this->settings = array(
+                'version' => '1.0.0',
+                'url'     => plugin_dir_url(__FILE__),
+                'path'    => plugin_dir_path(__FILE__),
+            );
+        }
+
+        public function init()
+        {
+            // set text domain
+            // https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
+            load_plugin_textdomain('acf-social_connection', false, plugin_basename(dirname(__FILE__) . '/..') . '/lang');
+
+            // include field
+            add_action('acf/include_field_types', array($this, 'include_field_types')); // v5
+            add_action('acf/register_fields', array($this, 'include_field_types')); // v4
+
+            NetworkApi::init();
+        }
+
+        /*
+        *  include_field_types
+        *
+        *  This function will include the field type class
+        *
+        *  @type	function
+        *  @date	17/02/2016
+        *  @since	1.0.0
+        *
+        *  @param	$version (int) major ACF version. Defaults to false
+        *  @return	n/a
+        */
+
+        function include_field_types($version = false)
+        {
+            // support empty $version
+            if (!$version) $version = 4;
+
+            // include
+            include_once('fields/acf-social_connection-v' . $version . '.php');
+        }
     }
-	
-	
-	/*
-	*  include_field_types
-	*
-	*  This function will include the field type class
-	*
-	*  @type	function
-	*  @date	17/02/2016
-	*  @since	1.0.0
-	*
-	*  @param	$version (int) major ACF version. Defaults to false
-	*  @return	n/a
-	*/
-	
-	function include_field_types( $version = false ) {
-		
-		// support empty $version
-		if( !$version ) $version = 4;
-		
-		
-		// include
-		include_once('fields/acf-social_connection-v' . $version . '.php');
-		
-	}
-	
-}
 
-
-// initialize
-new Networks();
-
+    // initialize
+    (new WpSocialNetwork())->init();
 
 // class_exists check
 endif;
-	
+
 ?>
